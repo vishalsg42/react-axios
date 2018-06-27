058a9b222e74deda79ad68ab196003948371055b
guptavishals <vishalsg@prdxn.com>
2018-06-27 22:06:31 +0530

    added GET request

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -4,18 +4,42 @@ import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
-
+import axios from 'axios';
 class Blog extends Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            posts: [],
+            selectedPostId: null
+        }
+    }
+    
+    postClicked= (id)=>{
+      this.setState({selectedPostId: id});
+    }
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts').then(res=> {
+          const updateValue = res.data.splice(0,4);
+          const updateNewValue = updateValue.map(post => {
+            return {
+              ...post,
+              author: 'max'
+            }
+          })
+           this.setState({posts: updateNewValue});
+        });
+    }
     render () {
+        const posts = this.state.posts.map((post,index) => {
+          return <Post key={post.id} title={post.title} author={post.author} clicked={()=>this.postClicked(post.id)} />
+        })
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                  {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost  id={this.state.selectedPostId}/>
                 </section>
                 <section>
                     <NewPost />

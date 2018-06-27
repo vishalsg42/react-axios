56341d2f823d9f7e41be68a926ca97dbd9611ee7
guptavishals <vishalsg@prdxn.com>
2018-06-27 22:33:22 +0530

    added new post

--- src/components/NewPost/NewPost.js
+++ src/components/NewPost/NewPost.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import './NewPost.css';
 
 class NewPost extends Component {
@@ -9,20 +9,37 @@ class NewPost extends Component {
         author: 'Max'
     }
 
+    postHandler= ()=> {
+        const post = {
+            title: this.state.title,
+            content: this.state.content,
+            author: this.state.author
+        }
+        axios.post('https://jsonplaceholder.typicode.com/posts/',post).then(response => {
+          console.log(response);
+        });
+    }
+
+    getInputData= (e,attr)=> {
+      console.log(e.target.value);
+      return  this.setState({attr: e.target.value});
+    }
+
     render () {
-        return (
+
+      return (
             <div className="NewPost">
                 <h1>Add a Post</h1>
                 <label>Title</label>
-                <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
+                <input type="text" value={this.state.title} onKeyUp={(event) => this.getInputData(event,'title') } />
                 <label>Content</label>
-                <textarea rows="4" value={this.state.content} onChange={(event) => this.setState({content: event.target.value})} />
+                <textarea rows="4" value={this.state.content}  onKeyUp={(event) => this.getInputData(event,'content') } />
                 <label>Author</label>
-                <select value={this.state.author} onChange={(event) => this.setState({author: event.target.value})}>
+                <select value={this.state.author}  onChange={(event) => this.getInputData(event,'author') } >
                     <option value="Max">Max</option>
                     <option value="Manu">Manu</option>
                 </select>
-                <button>Add Post</button>
+                <button onClick={()=> this.postHandler()}>Add Post</button>
             </div>
         );
     }

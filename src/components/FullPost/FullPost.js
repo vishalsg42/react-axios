3d33ec482641506671d1947d3e53c81bb0a09116
guptavishals <vishalsg@prdxn.com>
2018-06-28 14:09:43 +0530

    implemented interceptors

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -14,7 +14,6 @@ class FullPost extends Component {
       });
     }
     componentDidUpdate() {
-      console.log('did update',this.props.id);    
       if(this.props.id) {
         if(!this.state.loadedPost ||(this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
           axios.get('https://jsonplaceholder.typicode.com/posts/'+ this.props.id).then(response => {
--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+axios.interceptors.request.use(request => {
+    console.log(request);
+    return request;
+},error => {
+    console.log(error);
+    
+});
+
+axios.interceptors.response.use(response => {
+    console.log(response);
+    return response;
+});
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
